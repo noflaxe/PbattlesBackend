10b5216cb7c0d1356fc68fc0cb49e1a93fe6b3e3
Nazar Sheremeta <nsheremeta@cogniance.com>
2016-04-24 22:07:11 +0300

    Removed useless stuff

---
+++ pbattlesBackend/src/main/java/com/pbattles/main/Main.java
@@ -0,0 +1,32 @@
+package com.pbattles.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: noflaxe
+ * Date: 23.04.16
+ * Time: 23:18
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+
+@SpringBootApplication
+@ImportResource(value = "classpath:spring-config.xml")
+public class Main  extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Main.class);
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ pbattlesBackend/src/main/webapp/WEB-INF/pages/wtf.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<body>
+
+<br>
+JSTL URL:
+<br>
+Message:
+</body>
+
+</html>
